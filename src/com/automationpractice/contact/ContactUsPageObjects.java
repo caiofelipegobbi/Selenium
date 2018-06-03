98f9ca26704a51bca1263ecc7a2d295f8fae2107
Caio Felipe Gobbi <caiofelipegobbi@gmail.com>
2018-06-03 03:15:08 -0300

    Project created.

---
+++ src/com/automationpractice/contact/ContactUsPageObjects.java
@@ -0,0 +1,9 @@
+package com.automationpractice.contact;
+
+import com.automationpractice.utilities.TestSetup;
+
+public class ContactUsPageObjects extends TestSetup {
+
+    WebDriver driver;
+
+}
---
+++ src/com/automationpractice/utilities/TestSetup.java
@@ -0,0 +1,4 @@
+package com.automationpractice.utilities;
+
+public class TestSetup {
+}
